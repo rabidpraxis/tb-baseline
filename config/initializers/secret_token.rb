afb86ba7d5d875776e9526cc7e149160c50a3569
rabidpraxis <webster.kevin@gmail.com>
2013-09-14 03:33:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TbBaseline::Application.config.secret_token = 'd88514ce92d8a49c6f7cd0aaa1f2753581259f164bbf2250ceaba4fc5419fbe3e14b443524e74030079209f6dd525c98807bc8bdd6ff4352af9149acefd1e087'
