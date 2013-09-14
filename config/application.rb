35d3ff2db5b24805940806516a623b46f249083f
rabidpraxis <webster.kevin@gmail.com>
2013-09-14 05:15:37 -0700

    torquebox dependencies

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module TbBaseline
   class Application < Rails::Application
+      # Use TorqueBox::Infinispan::Cache for the Rails cache store
+  if defined? TorqueBox::Infinispan::Cache
+    config.cache_store = :torquebox_store
+  end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
